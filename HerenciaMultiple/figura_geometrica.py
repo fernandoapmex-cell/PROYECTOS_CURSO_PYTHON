43e97c0d53ea4794a6baf138878ef730eabf0558
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-04 23:37:39 -0600

    Actualizacion de curso

--- HerenciaMultiple/figura_geometrica.py
+++ HerenciaMultiple/figura_geometrica.py
@@ -1,11 +1,14 @@
-class FiguraGeometrica:
+#para hacer una clase abstracta tenemos que extender de la clase abc = Abstract Base Class
+from abc import ABC, abstractmethod
+
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho,alto):
-        if 0 < ancho < 10:
+        if self._validar_valor(ancho):
             self._ancho = ancho
         else:
             self._ancho = 0
-        self._alto = alto
-        if 0 < alto < 10:
+        if self._validar_valor(alto):
             self._alto = alto
         else:
             self._alto = 0
@@ -18,6 +21,13 @@ class FiguraGeometrica:
         self._ancho = ancho
     def setAlto(self, alto):
         self._alto = alto
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
     def __str__(self):
         return (f'Figura Geometrica:'
                 f'ancho: {self._ancho}, alto: {self._alto}')
+
+    def _validar_valor(self,valor):
+        return True if 0 < valor < 10 else False
--- HerenciaMultiple/test_figura_geometrica.py
+++ HerenciaMultiple/test_figura_geometrica.py
@@ -1,15 +1,17 @@
 from HerenciaMultiple.cuadrado import Cuadrado
+from HerenciaMultiple.figura_geometrica import FiguraGeometrica
 from HerenciaMultiple.rectangulo import Rectangulo
+#no podemos instanciar una clase abstracta
+#figura = FiguraGeometrica()
 print('creacion objeto cuadrado'.center(50,'-'))
 cuadrado1=Cuadrado(14,'Rojo')
-
-#print(cuadrado1.getAncho())
-#print(cuadrado1.getAlto())
-#print(cuadrado1.getColor())
-print(cuadrado1.calcular_area())
+cuadrado1._alto = -10
 print(cuadrado1)
 print('creacion objeto rectangulo'.center(50,'-'))
 rectangulo1 = Rectangulo(13,18,'Amarillo')
-print(rectangulo1.calcular_area())
 print(rectangulo1)
+#print(cuadrado1.getAncho())
+#print(cuadrado1.getAlto())
+#print(cuadrado1.getColor())
+
 #metodo MRO - Method Resolution Order|
