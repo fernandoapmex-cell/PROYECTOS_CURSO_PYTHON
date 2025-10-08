65fc2900642e647d3d8a4ce2814f46883682a3b9
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-08 02:33:07 -0600

    Actualizacion de curso

--- BasesDeDatos/conexion.py
+++ BasesDeDatos/conexion.py
@@ -1,4 +1,6 @@
-import psycopg2
+from contextlib import closing
+
+from psycopg2 import pool
 import sys
 from  logger_base import log
 class Conexion:
@@ -7,34 +9,19 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion=None
-    _cursor=None
+    _MIN_CONNECTIONS = 1
+    _MAX_CONNECTIONS = 5
+    _pool_=None
 
+    @classmethod
+    def obtener_pool(cls):
+        if cls._pool_ is None:
+            try:
+            except Exception as e:
+                log.error(f'Ocurrio un error al obtener el pool {e}')
     @classmethod
     def obtener_conexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = psycopg2.connect(host=cls._HOST,user=cls._USERNAME,password=cls._PASSWORD,port=cls._DB_PORT,dbname=cls._DATABASE)
-                log.debug(f'Conexion exitosa : {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio una exepcion al obtener la conexion:{e}')
-                sys.exit()
-        else:
-            return cls._conexion
-    @classmethod
-    def obtener_cursor(cls):
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtener_conexion().cursor()
-                log.debug(f'Se abrio el cursor correctamente : {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrio una exepcion al obtener el cursor:{e}')
-                sys.exit()
-        else:
-            return cls._cursor
+        pass
 
-if __name__ == '__main__':
-        #Conexion.obtener_conexion()
-        Conexion.obtener_cursor()
+
+
