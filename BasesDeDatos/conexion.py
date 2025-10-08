6f0e42348a1983dd0ae04da30032a00d040d3447
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-08 02:26:53 -0600

    Actualizacion de curso

--- BasesDeDatos/conexion.py
+++ BasesDeDatos/conexion.py
@@ -2,7 +2,7 @@ import psycopg2
 import sys
 from  logger_base import log
 class Conexion:
-    _DATABASE = 'test_dbs'
+    _DATABASE = 'test_db'
     _USERNAME = 'postgres'
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
@@ -18,8 +18,23 @@ class Conexion:
                 log.debug(f'Conexion exitosa : {cls._conexion}')
                 return cls._conexion
             except Exception as e:
-                log.debug(f'Ocurrio una exepcion {e}')
+                log.error(f'Ocurrio una exepcion al obtener la conexion:{e}')
                 sys.exit()
         else:
             return cls._conexion
     @classmethod
+    def obtener_cursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtener_conexion().cursor()
+                log.debug(f'Se abrio el cursor correctamente : {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrio una exepcion al obtener el cursor:{e}')
+                sys.exit()
+        else:
+            return cls._cursor
+
+if __name__ == '__main__':
+        #Conexion.obtener_conexion()
+        Conexion.obtener_cursor()
---
+++ BasesDeDatos/persona_dao.py
@@ -0,0 +1,75 @@
+from conexion import Conexion
+from persona import Persona
+from logger_base import log
+
+class PersonaDAO:
+    '''
+    DAO (Data Access Object)
+    CRUD (Create - Read -Update -Delete)
+
+
+    '''
+
+    _SELECCIONAR='SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR='INSERT INTO persona (nombre,apellido,email) VALUES (%s,%s,%s)'
+    _ACTUALIZAR='UPDATE persona SET nombre=%s,apellido=%s,email=%s WHERE id_persona=%s'
+    _ELIMINAR='DELETE FROM persona WHERE id_persona=%s'
+
+    @classmethod
+    def seleccionar(cls):
+        #usaremos un with para cerrar la conexion
+        with Conexion.obtener_conexion() as conexion:
+            with conexion.cursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas=[]
+                for registro in registros:
+                    #aca creamos el objeto de tipo persona para crear el objeto con los datos de la bd
+                    persona = Persona(registro[0],registro[1],registro[2],registro[3])
+                    personas.append(persona)
+                return personas
+
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtener_conexion():
+            with Conexion.obtener_cursor() as cursor:
+                valores=(persona.nombre,persona.apellido,persona.email)
+                cursor.execute(cls._INSERTAR,valores)
+                log.debug(f'Persona insertada: {persona}')
+                return cursor.rowcount
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtener_conexion():
+            with Conexion.obtener_cursor() as cursor:
+                valores= (persona.nombre,persona.apellido,persona.email,persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR,valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtener_conexion():
+            with Conexion.obtener_cursor() as cursor:
+                valores=(persona.id_persona,)
+                cursor.execute(cls._ELIMINAR,valores)
+                log.debug(f'Persona eliminada: {persona}')
+                return cursor.rowcount
+
+if __name__ == '__main__':
+    #insertar un registro
+    #persona1 = Persona(nombre='Pedro',apellido='najera',email='pnajera@mail.com')
+    #personas_insertadas=PersonaDAO.insertar(persona1)
+    #log.debug(f'Personas insertadas: {personas_insertadas}')
+
+    #actualizar un registro
+    # persona1 =Persona(26,'Jean patiño','Juarez','jpatiño@mail.com')
+    # personas_actualizadas=PersonaDAO.actualizar(persona1)
+    # log.debug(f'Personas Actualizadas: {personas_actualizadas}')
+
+    #eliminar un registro
+    persona1 = Persona(id_persona=25)
+    personas_eliminadas=PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+    #seleccionar objetos
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
