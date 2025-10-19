ce7038b9622c2d8c3fa6a8ee0d5f9bcaa7cbfa5b
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-19 01:47:05 -0600

    Actualizacion de curso

--- ProfundizandoPython/mro_ejemplo.py
+++ ProfundizandoPython/mro_ejemplo.py
@@ -8,8 +8,20 @@ class Clase2(Clase1):
         print('Clase2.__init__')
     def metodo(self):
         print('Metodo Clase2')
-class Clase3(Clase2):
+class Clase3(Clase1):
     def __init__(self):
         print('Clase3.__init__')
     def metodo(self):
         print('Metodo Clase3')
+class Clase4(Clase2,Clase3):
+    def metodo(self):
+        print('Metodo Clase4')
+        super().metodo()
+#objeto de clase 4
+clase4 = Clase4()
+#usamos bases para ver como se ejecuta el obj
+print(Clase4.__bases__)
+#usamos mro para ver como se ejecutan las clases hasta la chase padre
+print(Clase4.__mro__)
+#cual metodo se ejecuta
+clase4.metodo()
